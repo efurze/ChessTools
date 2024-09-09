58172e05254c425b1a12278f2914ad25f94eccba
efurze <efurze@gmail.com>
2024-09-09 10:00:11 -0700

    Manually copying over my changes from my non-working directory. This should mostly work.

---
+++ src/GeneratePositionHistory.ts
@@ -0,0 +1,245 @@
+/*
+    ImportPGN
+
+    USAGE: node GeneratePositionHistory.js outputdir/
+
+    This will create outputdir/positions, creating outputdir/ if neccessary. It assumes games are in outputdir/games
+*/
+
+import * as fs from 'fs';
+import * as readline from 'readline';
+import * as path from 'path';
+import { ChessGameState } from './ChessGameState';
+import * as crypto from 'crypto';
+const fsp = fs.promises;
+
+const GAME_DIR : string = "games";
+const POSITION_DIR : string = "positions";
+let INITIAL_IMPORT : boolean = false;
+
+class ScriptParams {
+
+    private output: string;
+   
+    public constructor(outputDir: string) {
+        this.output = outputDir;
+    }
+
+    public outputDir() : string {
+        return this.output;
+    }
+}
+
+
+
+
+function generateBase64Characters(): string[] {
+    const base64Chars = [];
+    // Uppercase letters (A-Z)
+    for (let i = 65; i <= 90; i++) {
+        base64Chars.push(String.fromCharCode(i));
+    }
+    // Lowercase letters (a-z)
+    for (let i = 97; i <= 122; i++) {
+        base64Chars.push(String.fromCharCode(i));
+    }
+    // Digits (0-9)
+    for (let i = 48; i <= 57; i++) {
+        base64Chars.push(String.fromCharCode(i));
+    }
+    // Special characters (+ and /)
+    base64Chars.push('-', '_');
+    return base64Chars;
+}
+
+function initializeOutputDirectory(outputdir : string) : void {
+    const positionsdir = path.join(outputdir, POSITION_DIR);
+
+    if (fs.existsSync(positionsdir)) {
+        INITIAL_IMPORT = false;
+        return;
+    } else {
+        INITIAL_IMPORT = true;
+    }
+
+    // make positions
+    const charSet = generateBase64Characters();
+    for (let i=0; i<charSet.length; i++) {
+        for (let j=0; j<charSet.length; j++) {
+            let dir = path.join(positionsdir, charSet[i] + charSet[j]);
+            fs.mkdirSync(dir, {recursive: true});
+        }
+    }
+}
+
+function readArgs(): ScriptParams {
+    const args = process.argv.slice(2); // first 2 args are node and this file
+    if (args.length < 1) {
+        console.log("Not enough parameters. USAGE: node GeneratePositionHistories.js outputdir/");
+        process.exit(1);
+    }
+
+    const params = new ScriptParams(args[0]);
+    return params;
+}
+
+ function loadPosition(posId : string, baseDir : string) : {[key:string] : string[]}  {
+    const filePath = path.join(baseDir, POSITION_DIR, posId.slice(0,2));
+    const fileName = posId.slice(2);
+    let data : string = "{}";
+    try {
+        const b : Buffer =  fs.readFileSync(path.join(filePath, fileName));
+        data = b ? b.toString() : "{}";
+        return JSON.parse(data);
+    } catch (err) {
+        //console.log(err);
+    }
+    return {};
+}
+
+
+ function savePosition(history : {[key:string] : string[]}, posId : string, baseDir : string) : void {
+    const filePath = path.join(baseDir, POSITION_DIR, posId.slice(0,2));
+    //console.log(filePath);
+    QueueManager.writeFile(filePath, posId.slice(2), JSON.stringify(history, null, " "));
+}
+
+
+ function updatePositionsForGame(game : ChessGameState, gameId : string, baseDir : string) : void {
+    try {
+        const moves = ChessGameState.parseMoves(game.getMeta("SAN"));
+        const positions = game.getBoardStates();
+        for (let i=0; i < positions.length-1; i++) { // no move is made in the last position
+            const posId = positions[i].toBase64();
+
+            /*
+                If we just created the directory then we know that loadPosition
+                will always fail, so just create an empty object. This speeds
+                the import up a lot.
+            */
+            const outData : {[key:string] : string[]} 
+                = INITIAL_IMPORT ? {} : loadPosition(posId, baseDir);
+
+            // check if we've already added this game to this position
+            if (!JSON.stringify(outData).includes(gameId)) {
+                // add this gameId to the position history
+                outData[moves[i]] = outData[moves[i]] ?? [];
+                outData[moves[i]].push(gameId);
+                savePosition(outData, posId, baseDir);
+            } 
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function* enumerateFiles(dir : string) : Generator<string> {
+  for (const entry of fs.readdirSync(dir)) {
+    const fullPath : string = path.join(dir, entry);
+    if (fs.statSync(fullPath).isDirectory()) {
+      yield* enumerateFiles(fullPath);
+    } else {
+      yield fullPath;
+    }
+  }
+}
+
+ function run(): void {
+    const params = readArgs();
+
+    console.log("Initializing destination directory");
+    initializeOutputDirectory(params.outputDir());
+
+    console.log("Reading game directory");
+    const filegenerator : Generator<string> = enumerateFiles(path.join(params.outputDir(), GAME_DIR));
+
+    QueueManager.init(params, filegenerator);
+    QueueManager.start();
+}
+
+class QueueManager {
+
+    
+    // This limits the number of open file descriptors
+    private static MAX_PENDING : number = 500;
+    private static MAX_BUFFER : number = 5000;
+    private static pendingWrites : number = 0;
+    private static buffer : {filepath:string, data:string}[] = [];
+    private static fileGenerator: Generator<string>;
+    private static params: ScriptParams;
+    private static gameCount: number = 0;
+    private static done: boolean = false;
+
+    private constructor() {}
+
+    public static init(params:ScriptParams, fg:Generator<string>) : void {
+        QueueManager.fileGenerator = fg;
+        QueueManager.params = params;
+    }
+
+    public static start() : void {
+        QueueManager.pump();
+    }
+
+    public static writeFile(dir:string, file:string, data:string) : void {
+        QueueManager.buffer.push({
+            filepath: path.join(dir, file),
+            data: data
+        });
+
+        QueueManager.pump();
+    }
+
+    private static readMore() : void {
+        const file : string | undefined = QueueManager.fileGenerator.next().value;
+        if (file) {
+            try {
+                const data : Buffer = fs.readFileSync(file);
+                const game = ChessGameState.fromJSON(JSON.parse(data.toString()));
+
+                const enclosingDir : string = path.basename(path.dirname(file));
+                const fileName : string = path.basename(file);
+                
+                updatePositionsForGame(game, enclosingDir + fileName, QueueManager.params.outputDir());
+                if (++QueueManager.gameCount % 10 == 0) {
+                    console.error("game " + QueueManager.gameCount);
+                }
+            } catch (err) {
+                console.log(err);
+                console.log(file);
+            }
+
+            QueueManager.pump();
+
+        } else {
+            QueueManager.done = true;
+            console.log("End of input. " + QueueManager.buffer.length + " writes remaining");
+        }
+    }
+
+    private static pump() : void {
+        setTimeout(function(){QueueManager.doPump();}, 0);
+    }
+
+    private static doPump() : void {
+        //console.log("doPump", "buffer:", QueueManager.buffer.length, "pending:", QueueManager.pendingWrites);
+        if (QueueManager.buffer.length < QueueManager.MAX_BUFFER) {
+            QueueManager.readMore();
+        }
+        if (QueueManager.buffer.length && QueueManager.pendingWrites < QueueManager.MAX_PENDING) {
+            QueueManager.pendingWrites++;
+            const toWrite : {filepath:string, data:string} | undefined = QueueManager.buffer.shift();
+            if (toWrite) {
+                fsp.writeFile(toWrite.filepath, toWrite.data)
+                    .finally(function() {
+                        QueueManager.pendingWrites--;
+                        QueueManager.pump();
+                    });
+            }
+        }
+        
+    }
+}
+
+console.log("Starting");
+run();
--- src/ImportPGN.ts
+++ src/ImportPGN.ts
@@ -14,8 +14,6 @@ import * as crypto from 'crypto';
 const fsp = fs.promises;
 
 const GAME_DIR : string = "games";
-const POSITION_DIR : string = "positions";
-let INITIAL_IMPORT : boolean = false;
 
 class ScriptParams {
 
@@ -40,7 +38,7 @@ class RateLimitedWriter {
 
     
     // This limits the number of open file descriptors
-    private static MAX_PENDING : number = 500;
+    private static MAX_PENDING : number = 1;
     private static pendingWrites : number = 0;
 
     private constructor() {}
@@ -61,47 +59,12 @@ class RateLimitedWriter {
 }
 
 
-function generateBase64Characters(): string[] {
-    const base64Chars = [];
-    // Uppercase letters (A-Z)
-    for (let i = 65; i <= 90; i++) {
-        base64Chars.push(String.fromCharCode(i));
-    }
-    // Lowercase letters (a-z)
-    for (let i = 97; i <= 122; i++) {
-        base64Chars.push(String.fromCharCode(i));
-    }
-    // Digits (0-9)
-    for (let i = 48; i <= 57; i++) {
-        base64Chars.push(String.fromCharCode(i));
-    }
-    // Special characters (+ and /)
-    base64Chars.push('-', '_');
-    return base64Chars;
-}
-
 function initializeOutputDirectory(outputdir : string) : void {
     const gamesdir = path.join(outputdir, GAME_DIR);
-    const positionsdir = path.join(outputdir, POSITION_DIR);
-    // make games
+   // make games
     for (let i=0; i<256; i++) {
         let dir = path.join(gamesdir, i.toString(16).padStart(2, '0'));
         fs.mkdirSync(dir, { recursive: true });
-    }
-
-    if (fs.existsSync(positionsdir)) {
-        INITIAL_IMPORT = false;
-    } else {
-        INITIAL_IMPORT = true;
-    }
-
-    // make positions
-    const charSet = generateBase64Characters();
-    for (let i=0; i<charSet.length; i++) {
-        for (let j=0; j<charSet.length; j++) {
-            let dir = path.join(positionsdir, charSet[i] + charSet[j]);
-            fs.mkdirSync(dir, {recursive: true});
-        }
     }
 }
 
@@ -116,54 +79,6 @@ function readArgs(): ScriptParams {
     return params;
 }
 
-async function loadPosition(posId : string, baseDir : string) : Promise<{[key:string] : string[]}>  {
-    const filePath = path.join(baseDir, POSITION_DIR, posId.slice(0,2));
-    const fileName = posId.slice(2);
-    let data : string = "{}";
-    try {
-        const b : Buffer = await fsp.readFile(path.join(filePath, fileName));
-        data = b ? b.toString() : "{}";
-        return JSON.parse(data);
-    } catch (err) {
-        //console.log(err);
-    }
-    return {};
-}
-
-
-async function savePosition(history : {[key:string] : string[]}, posId : string, baseDir : string) : Promise<void> {
-    const filePath = path.join(baseDir, POSITION_DIR, posId.slice(0,2));
-    await RateLimitedWriter.writeFile(filePath, posId.slice(2), JSON.stringify(history, null, " "));
-}
-
-
-async function importPositions(game : ChessGameState, gameId : string, baseDir : string) : Promise<void> {
-    try {
-        const moves = ChessGameState.parseMoves(game.getMeta("SAN"));
-        const positions = game.getBoardStates();
-        for (let i=0; i < positions.length-1; i++) { // no move is made in the last position
-            const posId = positions[i].toBase64();
-
-            /*
-                If we just created the directory then we know that loadPosition
-                will always fail, so just create an empty object. This speeds
-                the import up a lot.
-            */
-            const outData : {[key:string] : string[]} 
-                = INITIAL_IMPORT ? {} : await loadPosition(posId, baseDir);
-
-            // check if we've already added this game to this position
-            if (!JSON.stringify(outData).includes(gameId)) {
-                // add this gameId to the position history
-                outData[moves[i]] = outData[moves[i]] ?? [];
-                outData[moves[i]].push(gameId);
-                await savePosition(outData, posId, baseDir);
-            }           
-        }
-    } catch (err) {
-        console.log(err);
-    }
-}
 
 async function importGame(data : string[], baseDir : string) : Promise<void> {
     try {
@@ -176,17 +91,16 @@ async function importGame(data : string[], baseDir : string) : Promise<void> {
                         .digest('hex')
                         .slice(0,16);
 
-        // get the metadata
+
         let outdata: {[key: string]:string} = {};
-        game.getMetaKeys().forEach(function(key:string) {
+         game.getMetaKeys().forEach(function(key:string) {
             outdata[key] = game.getMeta(key);
-        })
-        // Save game file. Files are sharded by splitting the first 2 bytes of the hash
-        // 'e8e7c50fbff3c046' => 'e8/e7c50fbff3c046'
-        await RateLimitedWriter.writeFile(path.join(baseDir, GAME_DIR, hash.slice(0,2)),
-            hash.slice(2),
-            JSON.stringify(outdata, null, " "));
-        await importPositions(game, hash, baseDir);
+         })
+         // Save game file. Files are sharded by splitting the first 2 bytes of the hash
+         // 'e8e7c50fbff3c046' => 'e8/e7c50fbff3c046'
+         await RateLimitedWriter.writeFile(path.join(baseDir, GAME_DIR, hash.slice(0,2)),
+                                            hash.slice(2),
+                                            JSON.stringify(outdata, null, " "));
 
     } catch (err) {
         console.log(err);
